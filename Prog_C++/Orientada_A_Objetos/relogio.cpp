49a4bbd046cca7099b675824c077fc6fbe05d58c
Marcos André <marcos32andre@gmail.com>
2021-09-01 20:50:34 -0300

    A fim de deixar o projeto com cara mais profissional, foi criado agora um cabeçalho, o arquivo "relogio.h"

--- Prog_C++/Orientada_A_Objetos/relogio.cpp
+++ Prog_C++/Orientada_A_Objetos/relogio.cpp
@@ -1,17 +1,4 @@
-#include <iostream>
-#include <stdio.h>
-#include <windows.h>
-#include <iomanip>
-
-using namespace std;
-
-int hora;
-int minuto;
-int segundos=0;
-int dia;
-int mes;
-int ano;
-int aux;
+#include "relogio.h"
 
 int main()
 {
---
+++ Prog_C++/Orientada_A_Objetos/relogio.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <iomanip>
+
+using namespace std;
+
+int hora;
+int minuto;
+int segundos=0;
+int dia;
+int mes;
+int ano;
+int aux;
